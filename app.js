969b85c8864a678aa8352fb5b3a63601308469ab
EdoTO <edobusti@gmail.com>
2022-02-15 15:01:05 +0000

    update

---
+++ app.js
@@ -0,0 +1,20 @@
+
+
+const images = [
+    'img/01.jpg',
+    'img/02.jpg',
+    'img/03.jpg',
+    'img/04.jpg',
+    'img/05.jpg',
+];
+
+const slideContainer = document.querySelector('.slide-wrapper')
+
+console.log(slideContainer)
+
+for ( let i = 0; i < images.length; i++ ) {
+
+    const item =
+    slideContainer.innerHTML += item
+
+}
